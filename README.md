1f25e599fc713c033f3faf95aab8fbe5efd0897b
Andrey Popp <8mayday@gmail.com>
2026-01-04 13:40:40 +0400

    Updates ocaml-lsp-server 1.25.0
    It supports .mlx out of the box.

--- README.md
+++ README.md
@@ -1,44 +1,24 @@
 # ocaml_mlx.nvim
 
-[neovim][] plugin for [mlx][].
+[neovim][] plugin for OCaml's [.mlx][] syntax.
 
 ## Installation
 
 With [vim-plug][] (or your favorite plugin manager):
 ```vim
-Plug 'neovim/nvim-lspconfig'
-Plug 'nvim-treesitter/nvim-treesitter'
+Plug 'neovim/nvim-lspconfig'           -- for ocamllsp config
+Plug 'nvim-treesitter/nvim-treesitter' -- for treesitter support
 Plug 'ocaml-mlx/ocaml_mlx.nvim'
 ```
 
-Then in `init.lua` (before lsp setup):
+Then in `init.lua`:
 ```lua
 require 'ocaml_mlx'
 ```
 
 Then run `:TSInstall ocaml_mlx` to install the treesitter parser.
 
-### Support formatting with `ocamlformat-mlx`
-
-To enable formatting with [ocamlformat-mlx][] (not yet released on opam), install
-[stevearc/conform.nvim]:
-```vim
-Plug 'stevearc/conform.nvim'
-```
-
-Then in `init.lua`:
-```lua
-local conform = require 'conform'
-
-conform.setup {
-  formatters_by_ft = {
-    ocaml_mlx = {"ocamlformat_mlx"},
-  }
-}
-```
-
 [neovim]: https://neovim.io/
-[mlx]: https://github.com/ocaml-mlx/mlx
+[.mlx]: https://github.com/ocaml-mlx/mlx
 [vim-plug]: https://github.com/junegunn/vim-plug
-[stevearc/conform.nvim]: https://github.com/stevearc/conform.nvim
 [ocamlformat-mlx]: https://github.com/ocaml-mlx/ocamlformat-mlx
--- lua/ocaml_mlx.lua
+++ lua/ocaml_mlx.lua
@@ -1,8 +1,3 @@
-function can_require(module_name)
-  local ok,_ = pcall(require, module_name)
-  return ok
-end
-
 vim.filetype.add {
   extension = {
     mlx = 'ocaml.mlx'
@@ -11,44 +6,20 @@ vim.filetype.add {
 
 vim.treesitter.language.register("ocaml_mlx", "ocaml.mlx")
 
-if can_require 'lspconfig' then
-  local lspconfig = require 'lspconfig'
-  lspconfig.util.on_setup = lspconfig.util.add_hook_before(lspconfig.util.on_setup, function(config)
-    if config.name == 'ocamllsp' then
-      local filetypes = vim.deepcopy(config.filetypes)
-      table.insert(filetypes, 'ocaml.mlx')
-      config.filetypes = filetypes
-
-      local get_language_id = config.get_language_id
-      function config.get_language_id(bufnr, ft)
-        if ft == 'ocaml.mlx' then return 'ocaml'
-        else return get_language_id(bufnr, ft) end
-      end
-    end
-  end)
-end
-
-if can_require('nvim-treesitter') then
-  local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-  parser_config.ocaml_mlx = {
-    install_info = {
-      url = "https://github.com/ocaml-mlx/tree-sitter-mlx",
-      files = {'src/scanner.c', 'src/parser.c'},
-      location = 'grammars/mlx',
-      branch = 'master',
-      generate_requires_npm = false,
-      requires_generate_from_grammar = false,
-    },
-    filetype = 'ocaml_mlx',
-  }
-end
-
-if can_require('conform') then
-  local conform = require 'conform'
-  conform.formatters.ocamlformat_mlx = {
-    inherit = false,
-    command = 'ocamlformat-mlx',
-    args = { '--name', '$FILENAME', '--impl', '-' },
-    stdin = true,
-  }
-end
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'TSUpdate',
+  callback = function()
+    local parsers = require('nvim-treesitter.parsers')
+    parsers.ocaml_mlx = {
+      install_info = {
+        url = "https://github.com/ocaml-mlx/tree-sitter-mlx",
+        files = {'src/scanner.c', 'src/parser.c'},
+        location = 'grammars/mlx',
+        branch = 'master',
+        generate_requires_npm = false,
+        requires_generate_from_grammar = false,
+      },
+      filetype = 'ocaml_mlx',
+    }
+  end
+})
